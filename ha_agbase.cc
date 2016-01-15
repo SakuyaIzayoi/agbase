d347a749647b497b68b21a9a0c57fb3d572faa57
Alessandro Gangemi <agangemi@stevens.edu>
2016-01-15 13:55:21 -0500

    Whitespace

--- ha_agbase.cc
+++ ha_agbase.cc
@@ -116,7 +116,7 @@ err:
 }
 
 static handler* agbase_create_handler(handlerton *hton,
-                                       TABLE_SHARE *table, 
+                                       TABLE_SHARE *table,
                                        MEM_ROOT *mem_root)
 {
   return new (mem_root) ha_agbase(hton, table);
